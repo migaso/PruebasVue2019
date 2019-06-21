6b7bffbdd8eab2e79f793bb4d730594747140455
Migue <migaso2@hotmail.com>
2019-06-20 22:00:34 -0500

    sentencia v-for  y color segun el estado

--- js/principal.js
+++ js/principal.js
@@ -1,6 +1,10 @@
 var app = new Vue({
     el : '#app', //índica el id del elemento
     data: {  //con data interactuamos con el DOM
-        mensaje: 'Hola vue.js', 
+        lista : [
+            {nombre: 'Juan Carlos', promedio: '10', estado: false},
+            {nombre: 'Liliana Nataly', promedio: '17', estado: true},
+            {nombre: 'Luisito comunica', promedio: '20', estado: true},
+        ],
     }
 })
